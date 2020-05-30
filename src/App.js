23ec1cc300bd8799896ce782e68a961969265288
austen cote <coteausten@gmail.com>
2020-05-29 21:46:09 -0700

    got rooms to select and messages to disply on load -austen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react'
-
+// import socket from 'socket.io-client'
 import './App.css'
+
+function GetRooms (props) {
+  const rooms = props.messages.map(msg => msg.room)
+  const filtered = rooms.filter(room => room)
+  console.log(filtered, 'this is the rooms')
+  return <select onInput={props.handle} value={props.room}>
+    <option value=''>--Select a Room--</option>
+    {filtered.map(room => <option key={room} value={room}>{room}</option>)}
+             </select>
+}
 
 class App extends React.Component {
   constructor (props) {
@@ -9,11 +19,14 @@ class App extends React.Component {
       messages: [],
       nick: '',
       time: new Date(),
-      room: ''
+      room: '',
+      value: ''
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  callApi () {
+  componentDidMount () {
     fetch('/messages')
       .then(response => response.json())
       .then(data => {
@@ -24,23 +37,62 @@ class App extends React.Component {
           // this.state.messages.concat(the new message)
         })
       })
+      // .then(getRooms(this.state.messages))
+      .then(console.log(this.state, 'this is the state'))
   }
+
+  handleInput (event) {
+    this.setState({ room: event.target.value })
+    console.log(event.target.value)
+  }
+
+  handleChange (event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    console.log(this.state.value)
+    const message = { text: this.state.value, date: this.state.time }
+    this.setState({ messages: this.state.messages.concat(message) })
+
+    // changeRoom () {
+    //   if (the room === this.state.room) {
+    //     we want to show
+    //   }
+    // }
+
+    fetch('/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ text: this.state.value, date: this.state.time })
+    })
+      .then(data => {
+        console.log('Success:', data)
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+    // this.setState({ messages: this.state.messages.concat(message) })
+    // socket.emit('chat message', message)
+  }
+  // this.setState({ message: msg })
 
   render () {
     return (
       <div className='App'>
         <div id='chat-container' />
         <button onClick={() => this.callApi()}>Get Messages</button>
-        <select>
-          <option value="">--Select a Room--</option>
-        </select>
+        <GetRooms messages={this.state.messages} room={this.state.room} handle={this.handleInput.bind(this)} />
+        <form id='send-message' onSubmit={this.handleSubmit}>
+          <textarea id='message-text' value={this.state.value} onChange={this.handleChange} placeholder='message...' />
+          <button type='submit'>Send</button>
+        </form>
         <div>
           {this.state.messages.map((message, i) => <li key={i}>{(new Date(message.date)).toLocaleString()} ~ {message.text}</li>)}
         </div>
-        <form id='send-message'>
-          <input id='message-text' type='text' name='description' placeholder='message...' />
-          <button type='submit'>Send</button>
-        </form>
       </div>
     )
   }
