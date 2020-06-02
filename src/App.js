7a340a15743b14572072ecac0e3c9c14214b4882
austen cote <coteausten@gmail.com>
2020-06-01 19:51:30 -0700

    completed update room and post with new room -austen

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import './App.css'
 function GetRooms (props) {
   const rooms = props.messages.map(msg => msg.room)
   const filtered = rooms.filter(room => room)
-  console.log(filtered, 'this is the rooms')
+  const simplified = Array.from(new Set(filtered))
+  console.log(simplified, 'this is the rooms')
   return <select onInput={props.handle} value={props.room}>
     <option value=''>--Select a Room--</option>
-    {filtered.map(room => <option key={room} value={room}>{room}</option>)}
-             </select>
+    {simplified.map(room => <option key={room} value={room}>{room}</option>)}
+  </select>
+}
+
+function Chat (props) {
+  const message = props.messages.filter(msg => msg.room === props.room)
+  console.log(message, 'this is the chat message')
+  // const filtered = message.filter(msg => msg.room === room)
+  return <div class='roomChat'>
+    <ul>
+      {message.map(message => <li key={message}><span>{message.text}</span></li>)}
+    </ul></div>
 }
 
 class App extends React.Component {
@@ -20,10 +31,16 @@ class App extends React.Component {
       nick: '',
       time: new Date(),
       room: '',
-      value: ''
+      value: '',
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+  }
+
+  addRoom () {
+    const roomName = prompt('Enter a room name')
+    this.setState({ room: roomName })
+    console.log(this.state, 'This is the entered room name')
   }
 
   componentDidMount () {
@@ -53,25 +70,21 @@ class App extends React.Component {
   handleSubmit (event) {
     event.preventDefault()
     console.log(this.state.value)
-    const message = { text: this.state.value, date: this.state.time }
+    const message = { text: this.state.value, date: this.state.time, room: this.state.room }
     this.setState({ messages: this.state.messages.concat(message) })
-
-    // changeRoom () {
-    //   if (the room === this.state.room) {
-    //     we want to show
-    //   }
-    // }
-
+    console.log(message)
+ 
     fetch('/messages', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ text: this.state.value, date: this.state.time })
+      body: JSON.stringify({ text: this.state.value, date: this.state.time, room: this.state.room })
     })
       .then(data => {
         console.log('Success:', data)
       })
+      .then(this.state.value = '')
       .catch((error) => {
         console.error('Error:', error)
       })
@@ -84,15 +97,17 @@ class App extends React.Component {
     return (
       <div className='App'>
         <div id='chat-container' />
-        <button onClick={() => this.callApi()}>Get Messages</button>
+        <button onClick={() => this.addRoom()}>Add Room</button>
+        <p>{this.state.room}</p>
         <GetRooms messages={this.state.messages} room={this.state.room} handle={this.handleInput.bind(this)} />
+        <Chat messages={this.state.messages} room={this.state.room} />
         <form id='send-message' onSubmit={this.handleSubmit}>
           <textarea id='message-text' value={this.state.value} onChange={this.handleChange} placeholder='message...' />
           <button type='submit'>Send</button>
         </form>
-        <div>
+        {/* <div>
           {this.state.messages.map((message, i) => <li key={i}>{(new Date(message.date)).toLocaleString()} ~ {message.text}</li>)}
-        </div>
+        </div> */}
       </div>
     )
   }
